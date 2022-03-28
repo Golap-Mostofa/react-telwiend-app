a92a2ea267acfd2cdbea2299059774f8c8c4ccdb
Golap-mostofa <mostofa6410@gmail.com>
2022-03-28 16:08:18 +0600

    ul-li-fixt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Pricing from './components/Pricing/Pricing';
+import Myline from './components/Myline/Myline';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
      <Pricing></Pricing>
+     <Myline></Myline>
     </div>
   );
 }
